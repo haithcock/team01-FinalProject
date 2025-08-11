c12a8518587a598beb17f68f251405ef57d24984
haithcock <dakota.haith1997@.com>
2025-08-11 00:29:56 -0400

    katherine and I changed some more things pt2

--- server.js
+++ server.js
@@ -46,8 +46,7 @@ passport.deserializeUser((user, done) => {
 });
 
 app.get('/github/callback', passport.authenticate('github', { failureRedirect: '/api-docs', session: false}), (req, res) => {
-    req.session.user = username;
-    console.log(req.session.username);
+    req.session.user = req.username;
     res.redirect('/'); // Redirect to home page after successful authentication
 });
 
